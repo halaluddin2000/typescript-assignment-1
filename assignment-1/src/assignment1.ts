6f1897f6d732d722f3fca1be8098f3e39744666d
halaluddin2000 <helaluddin7001@gmail.com>
2025-05-11 18:11:08 +0600

    problem 2

--- assignment-1/src/assignment1.ts
+++ assignment-1/src/assignment1.ts
@@ -5,6 +5,20 @@ function formatString(input: string, toUpper?: boolean = true): string {
     return input.toLowerCase();
   }
 }
-console.log(formatString("hello"));
-console.log(formatString("hello", true));
-console.log(formatString("hello", false));
+// console.log(formatString("hello"));
+// console.log(formatString("hello", true));
+// console.log(formatString("hello", false));
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.5 },
+  { title: "Book C", rating: 5.0 },
+];
+
+const filterByBooks = filterByRating(books);
+console.log(filterByBooks);
